61f998c698037d379972324e6f73d864bd87eac7
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-02 10:38:37 -0300

    se trabaja en estilos de mensajes

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import os
-from dotenv import load_dotenv
 import requests
-import xmlrpc.client
+from dotenv import load_dotenv
 
 class TelegramBot:
     def __init__(self, bot_token, chat_id):
         self.bot_token = bot_token
         self.chat_id = chat_id
         self.base_url = f'https://api.telegram.org/bot{self.bot_token}'
-        self.verificar()
 
-    def verificar(self):
-
-    
-    def enviar_mensaje_telegram(self):
-        
-        pass
-
-    def send_message(self, message):
+    def send_message(self, message, parse_mode="Markdown"):
         url = f'{self.base_url}/sendMessage'
         data = {
             'chat_id': self.chat_id,
-            'text': message
+            'text': message,
+            'parse_mode': parse_mode  # Usar Markdown o HTML
         }
         response = requests.post(url, data=data)
+        print(response.json())
         return response.json()
-    
+
+    def enviar_tabla(self):
+        # Ejemplo de datos
+        productos = [
+            {"Producto": "Producto A", "Stock": 5, "Mínimo": 10, "Ubicación": "Almacén 1"},
+            {"Producto": "Producto B", "Stock": 15, "Mínimo": 10, "Ubicación": "Almacén 2"},
+            {"Producto": "Producto C", "Stock": 0, "Mínimo": 5, "Ubicación": "Almacén 3"},
+        ]
+
+        # Crear el mensaje con formato Markdown
+        mensaje = "*Reporte de Productos*\n\n"
+        mensaje += "`{:<20} {:<10} {:<10} {:<15}`\n".format("Producto", "Stock", "Mínimo", "Ubicación")
+        mensaje += "`{:<20} {:<10} {:<10} {:<15}`\n".format("-" * 20, "-" * 10, "-" * 10, "-" * 15)
+
+        for producto in productos:
+            mensaje += "`{:<20} {:<10} {:<10} {:<15}`\n".format(
+                producto["Producto"], producto["Stock"], producto["Mínimo"], producto["Ubicación"]
+            )
+
+        # Enviar el mensaje
+        self.send_message(mensaje, parse_mode="Markdown")
 
 if __name__ == "__main__":
     # Cargar claves desde .env
@@ -36,4 +49,5 @@ if __name__ == "__main__":
     # Inicializar y enviar mensaje
     bot = TelegramBot(bot_token, chat_id)
     bot.enviar_tabla()
+
 
